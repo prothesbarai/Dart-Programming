18e24509156752a8460593b35b79028f1d27e515
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-05-10 08:45:07 +0600

    v1.1.1

--- C1 Basic/operator_in_dart.dart
+++ C1 Basic/operator_in_dart.dart
@@ -6,7 +6,10 @@ void main(){
   int a = 110, b = 60, sum;  // Fixed Data Type...
   sum = a + b;
   var sub = a-b;  // Dynamicly Choose Data Type ....
-  print(sum);
-  print(sub);
+  double c = 50.9904 , d = 20.6484;  //  Fixed Choose
+  var mul = c*d;
+  print("Integer = $sum");
+  print("var = $sub");
+  print("double = $mul");
 
 }
