58eaf196b1a5e764bca27c189f5d272e6d6581ed
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-05-10 13:42:22 +0600

    v1.1.5

--- C1 Basic/operator_with_Datatype.dart
+++ C1 Basic/operator_with_Datatype.dart
@@ -30,13 +30,40 @@ void main(){
   print("var = $bol2");
 
   // >>> ========== List Start Here ==========
-  var list = [1,2,3,4,5,6,7];
-  print("List = $list");
+  List<int> list1 = [1,2,3,4,5,6,7];
+  List<String> list2 = ["Apple","Ball","Cat","Filled"];
+  List<dynamic> list3 = [1,"Apple",true,3.88];
+
+  print("List = $list1");
   var pos = 3;
-  var listPos = list[pos];
-  print("List $pos Position value is = $listPos");
-  print("Total List Length = ${list.length}");
+  var listPos = list1[pos];
+  print("List1 $pos Position value is = $listPos");
+  print("Total List1 Length = ${list1.length}");
+
+  var itm = "New Items";
+  list2.add(itm);
+  print("Add $itm to list2 = $list2");
+  List<String> list4 = ["Banana","Orange","Mango"];
+  list2.addAll(list4);
+  print("New List list2 = $list2");
+  var reverse_order = list2.reversed;
+  print("Reverse List = $reverse_order");
+  var ritm = "Mango";
+  var remvb = list2.remove(ritm);
+  print("Remove $ritm from list2 : $remvb so final list is $list2");
+  
+  var rmv_index = 4;
+  list2.removeAt(rmv_index);
+  print("Remove $rmv_index Number Index Item : So Final List is $list2");
+
+  list2.removeLast();
+  print("Remove Last Item from list : $list2");
+
+  var st = 1;
+  var ed = 3;
+  list2.removeRange(st, ed);  // 1 number index theke 2 number index projonto Remove hobe
+  print("Remove $st to $ed Index Number of Items : So Final List is : $list2");
   // <<< ========== List End Here ==========
 
-  
+
 }
