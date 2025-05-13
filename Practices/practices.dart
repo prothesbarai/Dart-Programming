f52f6efc3ddfe715c0b1ff3d5553de74a409116f
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-05-13 08:42:59 +0600

    v1.2.6

--- Practices/practices.dart
+++ Practices/practices.dart
@@ -2,6 +2,8 @@ void main(){
   List<String> myList1 = ["Apple","Banana","Orange","Painaple","Tomato","Mango"];
   List<int> myList2 = [100,99,1,2,3,4,21,23,5,6,7,8,9,89,56];
 
+  var l1 = myList1.reversed.toList();
+  print(l1);
   print(myList1);
   myList1.add("new items");
   print(myList1);
@@ -23,7 +25,19 @@ void main(){
 
   myList2.sort();
   print(myList2);
+
   myList2.shuffle();
   print(myList2);
 
+
+  myList2.removeLast();
+  print(myList2);
+  myList2.remove(100);
+  print(myList2);
+  myList2.removeAt(1);
+  print(myList2);
+  myList2.removeRange(1, 5);
+  print(myList2);
+
+  
 }
