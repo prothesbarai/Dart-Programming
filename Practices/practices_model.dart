6704a375bf92a4a00f6c2190a2a0d045a9c053c4
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-06-17 08:30:43 +0600

    v1.2.3

---
+++ Practices/practices_model.dart
@@ -0,0 +1,11 @@
+class PracticesModel {
+
+    final int id;
+    final DateTime date;
+
+    PracticesModel({
+        required this.id, 
+        required this.date
+    });
+    
+}
---
+++ Practices/run_practices.dart
@@ -0,0 +1,12 @@
+import 'practices_model.dart';
+
+void main(){
+  
+
+  List<PracticesModel> myModel = [];
+
+
+  
+
+
+}
