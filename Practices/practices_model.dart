db3cb36fa2b09f7d66c217b4444859bf90da590b
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-06-22 08:35:42 +0600

    v1.1.1

---
+++ Practices/practices_model.dart
@@ -0,0 +1,11 @@
+class PracticesModel {    
+    final int id;
+    final String name;
+    final DateTime date;
+
+    PracticesModel({
+        required this.id,
+        required this.name,
+        required this.date,
+    });
+}
